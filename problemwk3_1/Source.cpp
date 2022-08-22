c6b6ea92f1bb1adf749ea13336b3083d4d84a6ac
Taarrz <110449799+Taarrz@users.noreply.github.com>
2022-08-23 01:15:32 +0700

    Update Source.cpp

--- problemwk3_1/Source.cpp
+++ problemwk3_1/Source.cpp
@@ -3,16 +3,21 @@ int main() {
 	int x,d,a;
 	scanf_s("%d", &x);
 	int i = 1;
-	while (i<=3) {
-		a = x % 1000;
-		x = x*10;
-		if (i != 3) {
-			printf("%d , ", a / 100);
+	if (x > 100) {
+		while (i <= 3) {
+			a = x % 1000;
+			x = x * 10;
+			if (i != 3) {
+				printf("%d , ", a / 100);
+			}
+			else {
+				printf("%d", a / 100);
+			}
+			i++;
 		}
-		else {
-			printf("%d", a / 100);
-		}
-		i++;
+	}
+	else {
+		printf("error");
 	}
 	return 0;
 }
